ea73dbf4159cdbb15d592c896ade3635a097f6d7
Perri Lapidus <plapidus@gmail.com>
2013-02-15 12:25:15 -0500

    experimenting with fancybox

--- scripts/jquery.js
+++ scripts/jquery.js
@@ -1,12 +1,6 @@
 $(document).ready(function(){
-
-
-
-
-jQuery(window).resize(function() {
-  var width = jQuery(document).width();
-  var margin = (833-width)/-2;
-  jQuery('.logo').css('margin-left', (margin)+'px');
-});
+  $('.iframe').fancybox({
+    fitToView: false
+  });
 
 });
